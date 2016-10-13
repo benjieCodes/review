0cd32eb337fffd58507a8f32affb013890f1e690
Benjie Songsong <benjiesongsong@gmail.com>
2016-10-13 14:24:26 -0400

    added a filter iteration function

--- main.js
+++ main.js
@@ -20,28 +20,24 @@
     }
   ];
 
-  var p2 = players[1];
-  var p2Skills = p2.skills
-  var p2Magic = p2Skills.magic;
-
   // console.log(p2Magic);
   // console.log(players[1].skills.magic);
 
-  // forEach does not return something
 
+  // forEach usage (does something)
   players.forEach( function (player) {
     console.log(player.skills.weapon);
   });
 
-  // map returns an array
-  
+  // map usage (returns something [an array])
   var weapons = players.map(function (player) {
     return player.skills.weapon;
   });
   console.log(weapons);
 
-
-
-
-
+  // filter usage (returns something [filtered array])
+  var wizards = players.filter(function (player) {
+    return player.skills.magic === 'Wizard';
+  });
+  console.log(wizards);
 }());
