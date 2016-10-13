2c0b4226bd968e3c21e63a9661f143b6987ea94c
Benjie Songsong <benjiesongsong@gmail.com>
2016-10-13 14:20:03 -0400

    review iteration functions difference between forEach and map

--- main.js
+++ main.js
@@ -1,11 +1,47 @@
 (function() {
   'use strict';
-  console.log(this);
 
-  var myVar = 'my variable name';
+  var players = [
+    {
+      name: 'player 1',
+      health: 100,
+      skills: {
+        weapon: 'Axe',
+        magic: 'Wizard'
+      }
+    },
+    {
+      name: 'player 2',
+      health: 100,
+      skills: {
+        weapon: 'Staff',
+        magic: 'Sorcerer'
+      }
+    }
+  ];
 
-  function something () {
-    var tiyWhiteBoard = "Some text here";
-  }
+  var p2 = players[1];
+  var p2Skills = p2.skills
+  var p2Magic = p2Skills.magic;
+
+  // console.log(p2Magic);
+  // console.log(players[1].skills.magic);
+
+  // forEach does not return something
+
+  players.forEach( function (player) {
+    console.log(player.skills.weapon);
+  });
+
+  // map returns an array
+  
+  var weapons = players.map(function (player) {
+    return player.skills.weapon;
+  });
+  console.log(weapons);
+
+
+
+
 
 }());
